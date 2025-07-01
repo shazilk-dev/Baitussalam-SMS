752bbcefd67dcfcb3b21b42eeec3b36d61ef3a55
ShazilK47 <shazil.akn@gmail.com>
2025-07-01 10:11:08 +0500

    added data

---
+++ data/students.js
@@ -0,0 +1,19 @@
+// data/students.js
+const students = [
+  {
+    id: "1",
+    name: "Ayaan Ahmed",
+    email: "ayaan@example.com",
+    age: 20,
+    course: "Web Dev",
+  },
+  {
+    id: "2",
+    name: "Sara Khan",
+    email: "sara@example.com",
+    age: 22,
+    course: "AI",
+  },
+];
+
+module.exports = students;
